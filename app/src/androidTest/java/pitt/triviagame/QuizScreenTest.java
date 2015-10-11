edf47b19e96b79caea278493ab3f2457cabdaacd
jda34 <jda34@pitt.edu>
2015-10-11 15:20:12 -0400

    added tests and made sure they work

---
+++ app/src/androidTest/java/pitt/triviagame/QuizScreenTest.java
@@ -0,0 +1,52 @@
+package pitt.triviagame;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.SmallTest;
+import android.widget.Button;
+import android.widget.TextView;
+
+import junit.framework.TestCase;
+
+import pitt.triviagame.R;
+import pitt.triviagame.QuizScreen;
+
+/**
+ * Created by Danny on 10/11/2015.
+ */
+public class QuizScreenTest extends ActivityInstrumentationTestCase2<QuizScreen>{
+
+    QuizScreen screen;
+
+    public QuizScreenTest(){
+        super(QuizScreen.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        screen = getActivity();
+    }
+
+    @SmallTest
+    public void testTextViewNotNull(){
+        TextView textView = (TextView)  screen.findViewById(R.id.timeLeftView);
+        assertNotNull(textView);
+    }
+
+    @SmallTest
+    public void testThatButton1HasCorrectAnswerAndCanIncrementPoints(){
+        int expectedPoints=1;
+        int points=0;
+
+        Button button1 = (Button)screen.findViewById(R.id.answerButton1);
+        String numString = button1.getText().toString();
+        int num = Integer.parseInt(numString);
+        if (num==4){
+            points++;
+        }
+        assertEquals(expectedPoints, points);
+
+
+    }
+
+}
