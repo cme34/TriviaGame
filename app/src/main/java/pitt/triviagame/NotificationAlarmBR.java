8d4b2cf03bffa1377ed9173d00101abb066f4142
ifl2 <ifl2@pitt.edu>
2015-11-10 14:47:30 -0500

    refactor

--- app/src/main/java/pitt/triviagame/NotificationAlarmBR.java
+++ app/src/main/java/pitt/triviagame/NotificationAlarmBR.java
@@ -12,48 +12,39 @@ import java.util.GregorianCalendar;
 
 public class NotificationAlarmBR extends BroadcastReceiver {
 
-//Leonard Aronson code review: I like the use of comments in your code however, you may want to refactor in order to 
-//make your code slightly more readable.
-
     @Override
-    // When alarm broadcast is received...
+    /** When alarm broadcast is received, wake and send notification */
     public void onReceive(Context context, Intent intent) {
-        // Create a temporary partial wake from lock to push notification through
         PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
         PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "Trivia Lock");
-        wakeLock.acquire(); // Acquire the lock
-        new TriviaGame().sendNotification(context); // Trigger sending notification, pass context
-        wakeLock.release(); // Release the lock
+        wakeLock.acquire();
+        new TriviaGame().sendNotification(context);
+        wakeLock.release();
     }
 
-    // Creates the notification alarm
+    /** Create notification alarm at specified time and time interval */
     public void SetAlarm(Context context) {
         int hour = 10;  // TODO: implement to change in settings
-        GregorianCalendar calendar = new GregorianCalendar(); // Create gregorian calendar
-        calendar.setTimeInMillis(System.currentTimeMillis()); // Set date to current system time
-        // If the current time is past the alarm time then set alarm for tomorrow,
-        // otherwise the alarm will think it has passed and end up triggering on the next minute
-        if (calendar.get(Calendar.HOUR_OF_DAY) >= hour)
+
+        // Calendar setup
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        if (calendar.get(Calendar.HOUR_OF_DAY) >= hour) // If current time has passed alarm time, set for tomorrow
             calendar.set(Calendar.DAY_OF_YEAR, calendar.get(Calendar.DAY_OF_YEAR) + 1);
-            
-        //Leonard Aronson code review:  I think it may be overkill to use these small mesurements in your code.  I do not
-        // think you need to set these fields.
-        // Now set calendar time to the set notification hour at 0 minutes 0 seconds
         calendar.set(Calendar.HOUR_OF_DAY, hour);
         calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-        // Setup the alarm manager
+
+        // Alarm Manager setup
         AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context, NotificationAlarmBR.class);
-        // Set the intent to broadcast
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+
         // Start at the set notification hour and repeat every 24 hours
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), (1000 * 60 * 60 * 24), pendingIntent);
     }
 
     // TODO turn off alarm if notification disabled in settings
-    // Disables the notification alarm
+    /** Disables the notification alarm */
     public void CancelAlarm(Context context) {
         Intent intent = new Intent(context, NotificationAlarmBR.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
