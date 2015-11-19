eff039a72fd630d8fdc2a729875177a5b132c6a4
cme34 <cme34@pitt.edu>
2015-11-19 15:53:41 -0500

    [Sprint 5][Commit 1] Added Database connection, and fixed some text sizes and minor details

--- app/src/main/java/pitt/triviagame/SettingsScreen.java
+++ app/src/main/java/pitt/triviagame/SettingsScreen.java
@@ -1,8 +1,12 @@
 package pitt.triviagame;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 /**
  * Created by Cory on 11/7/2015.
