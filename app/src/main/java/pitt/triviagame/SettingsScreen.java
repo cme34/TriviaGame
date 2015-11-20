50ea6e952dd764bbeae8e6e4216551103e90c8d9
cme34 <cme34@pitt.edu>
2015-11-19 21:37:10 -0500

    Reapplied Ian's Commit For Notification Switch

--- app/src/main/java/pitt/triviagame/SettingsScreen.java
+++ app/src/main/java/pitt/triviagame/SettingsScreen.java
@@ -1,18 +1,20 @@
 package pitt.triviagame;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Switch;
 
 /**
  * Created by Cory on 11/7/2015.
  * This page allows the user to edit app settings
  */
 public class SettingsScreen extends AppCompatActivity {
+
+    public static Switch notifySwitch;
 
     /**
      * This method is essentially a constructor. It initializes the trivia game (main menu screen)
@@ -22,7 +24,18 @@ public class SettingsScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
+	notifySwitch = (Switch) findViewById(R.id.notificationSwitch);
+        notifySwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            /** On notificationSwitch switch, set or cancel notification alarm */
+            @Override
+            // TODO: Have setting persist when app is closed
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                    new NotificationAlarmBR().SetAlarm(getBaseContext());
+                else
+                    new NotificationAlarmBR().CancelAlarm(getBaseContext());
+            }
+        });
     }
 
     /**
